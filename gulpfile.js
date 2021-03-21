33812e79f84dd75ada5cc9cccee38c73696af451
Илья <bartenev.ilya@gmail.com>
2021-03-21 14:00:39 +0300

    Добавляет webp изображения на index.html и catalog.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,35 @@
 const gulp = require("gulp");
+
+const htmlmin = require("gulp-htmlmin");
+
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
 const less = require("gulp-less");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const csso = require("gulp-csso");
+
 const rename = require("gulp-rename");
+const del = require("del");
+
+const uglify = require("gulp-uglify");
+const pipeline = require("readable-stream").pipeline;
 
 const imagemin = require("gulp-imagemin");
 const webp = require("gulp-webp")
 const svgstore = require("gulp-svgstore")
 
 const sync = require("browser-sync").create();
+
+// HTML
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"))
+}
+
+exports.html = html;
 
 // Styles
 
@@ -33,6 +51,18 @@ const styles = () => {
 
 exports.styles = styles;
 
+// JavaScript
+
+const js = () => {
+  return pipeline(
+        gulp.src("source/js/*.js"),
+        uglify(),
+        gulp.dest("build/js")
+  );
+}
+
+exports.js = js;
+
 // Images
 
 const images = () => {
@@ -40,8 +70,14 @@ const images = () => {
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.mozjpeg({progressive: true}),
-      imagemin.svgo(),
+      imagemin.svgo({
+        plugins: [
+            {removeViewBox: false},
+            {cleanupIDs: false}
+        ]
+      }),
     ]))
+    .pipe(gulp.dest("build/img"))
 }
 
 exports.images = images;
@@ -67,6 +103,32 @@ const sprite = () => {
 
 exports.sprite = sprite;
 
+// Clean
+
+const clean = () => {
+  return del("build")
+}
+
+exports.clean = clean;
+
+// Copy
+
+const copy = () => {
+  return gulp.src([
+    "source/fonts/**/*.{woff,woff2}",
+    "source/img/**/*.webp",
+    "source/img/**/sprite.svg",
+    "source/css/**/*.{min.css,min.css.map}",
+    "source/*.ico"
+    ] , {
+      base: "source"
+    })
+
+    .pipe(gulp.dest("build"))
+}
+
+exports.copy = copy;
+
 // Server
 
 const server = (done) => {
@@ -88,8 +150,21 @@ exports.server = server;
 const watcher = () => {
   gulp.watch("source/less/**/*.less", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
+  // gulp.watch("source/*.html", gulp.series("html")).on("change", sync.reload);
 }
+
+// Default
 
 exports.default = gulp.series(
   styles, server, watcher
+  // clean, copy, html, styles, server, watcher
 );
+
+// Build
+
+const build = gulp.series(
+  clean, images, createWebp, sprite, styles, html, js, copy
+);
+
+exports.build = build;
+
